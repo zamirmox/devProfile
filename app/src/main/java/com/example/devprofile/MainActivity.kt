efe6e1336595c1ccc2b71ae3503e9336931b310e
zamir <zamir@blueyonder.co.uk>
2021-07-03 21:19:18 +0100

    Lesson 4 tabled landscape

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+//        val  rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+//        rounded.cornerRadius = 15f
+//        profileimg.setImageDrawable(rounded)
+
     }
 }
